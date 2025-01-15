bf807f66ce3f32b85fbbc5b31bf7505a8a04f153
Ivan <ivan@192.168.1.7>
2025-01-15 19:12:47 +0100

    modul2

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,32 @@
-function getElementWidth(content, padding, border) {
-  const contentWidth = parseFloat(content);
-  const paddingWidth = parseFloat(padding) * 2;
-  const borderWidth = parseFloat(border) * 2;
-  const result = contentWidth + paddingWidth + borderWidth;
-  return result;
+function checkForSpam(message) {
+  let formatMessage = message.toLowerCase();
+  return formatMessage.includes('spam') || formatMessage.includes('sale');
 }
 
-// !===================================
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+
+
+
+
+
+
+
+
+
+
+// Доповни код функції таким чином, що:
+
+// Якщо знайдено заборонене слово (spam або sale), то функція повертає буль true
+// Якщо в рядку відсутні заборонені слова, функція повертає буль false
+
+
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+
+// !=============================================
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,38 @@
+function getShippingCost(country) {
+    let price;
+    switch (country) {
+        case 'China':
+            price = 100;
+        case 'Chile':
+            price = 250;
+        case 'Australia':
+            price = 170;
+        case 'Jamaica':
+            price = 120;
+            break;
+        default: "Sorry, there is no delivery to your country"
+            break;
+    }
+    return `Shipping to ${country} will cost ${price} credits`
+}
 
+
+
+// Формат рядка, що повертається "Shipping to <country> will cost <price> credits", де замість <country> і <price> необхідно підставити відповідні значення.
+
+// Список країн і вартість доставки:
+// China — 100 кредитів
+// Chile — 250 кредитів
+// Australia — 170 кредитів
+// Jamaica — 120 кредитів
+
+// Зі списку видно, що доставка можлива не скрізь. Якщо зазначена країна відсутня у списку, то функція повинна повернути рядок "Sorry, there is no delivery to your country".
+
+
+// !=================================================
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
